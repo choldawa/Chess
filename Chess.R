b0727f4443d642d2e1a104440bff8234e9e7de82
Cameron Holdaway <cameron.holdaway@gmail.com>
2020-06-16 16:36:20 -0700

    pseudocode

--- Chess.R
+++ Chess.R
@@ -1,3 +1,19 @@
+# PseudoCode:
+#   load pgn viarchess system.file?
+# get list of all moves in the game via chss$history()
+#   store this as an array? 
+# initialize blank chess game via chss = Chess$new()
+#   for each move in history:
+#     push move to blank game via chss$move("Nbd7")
+#   get all legal moves from there via chss$moves()
+#     for each possible in moves:
+#       push possible to hypothetical board via chss$move(possible)
+#       get stockfish eval of that position <----- unsure on this, tbd
+#       save stockfish eval to new array specific to this move
+#       remove possible from board via chss$undo()
+# take variance of each array of stockfish possibles for each move      
+
+
 #install.packages("rchess")
 #devtools::install_github("hrbrmstr/pigeon")
 install.packages("rlang", dependencies = TRUE) 
@@ -12,21 +28,37 @@ cat(pgn)
 
 chss = Chess$new()
 chss$load_pgn(pgn)
-chss$history()
-df_hist = chss$moves(verbose = TRUE)
-df_hist
+
+
+
 plot(chss)
-#legal moves
+#list of legal moves
 chss$moves()
+chss$moves(verbose = TRUE)
+
+chss$history()
+chss$history_detail()
+#get fen 
+chss$fen()
+#add a new move to board
+chss$move("Nbd7")
+#moves can be concatenated, e.g. below
+#chss$move("e5")$move("f4")$move("Qe7")$move("fxe5")
+chss$undo()
+plot(chss)
 
 
 
+
+
+#load game from pgn string
 pgn <- "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6
 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6
 8. c3 O-O 9. h3 Nb8  10. d4 Nbd7
 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7
 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5
 Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4"
+#chss <- Chess$new(--add FEN here to load specific position--)
 chss <- Chess$new()
 chss$load_pgn(pgn)
 
